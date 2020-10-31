ce3ef8734dce9b53e146a79358da587e34a0af95
Shemar Jones <47797339+S-TJones@users.noreply.github.com>
2020-10-31 08:42:25 -0500

    Completed part 2
    Completed the countWord function. Added the neccessary try-catch error preventions to make it run more smoothly.

--- Py Files/FileManipulation.py
+++ Py Files/FileManipulation.py
@@ -16,37 +16,94 @@ def checkLength():
     # Asks user for file location
     file_path = input(
         "\n*  PLease enter the file path to the file that you want to check the length of.\n")
-
     # Asks user for line number
     line_number = input(
         "*  Please enter the line number that you want the length for - ")
     line_number = int(line_number)
 
-    length = 0
+    length = -1  # Initialize variable to prevent errors
 
-    with open(os.path.join(file_path), "r") as file:
+    try:
+        with open(os.path.join(file_path), "r") as file:  # Opens the file for reading
 
-        all_lines = file.readlines()
+            all_lines = file.readlines()  # Gets all lines as a list of strings
 
-        num_lines = len(all_lines)
+            num_lines = len(all_lines)  # Total of all lines in the file
+
+            if num_lines < line_number:
+                print(f"This file doesn't have {line_number} lines.")
+            else:
+                specific_line = all_lines[line_number-1]
+                # Calculates the length of the specific line
+                length = len(specific_line)
+
+    except FileNotFoundError:
+        print("\nWrong file or file path.")
+        print("Remember to add an extension to file you entered.")
+    except IOError as e:
+        print("\nThere is an Error\n", e)
+    else:
+        print("No exceptions.\nUnknown Error found.")
+
+    # Checks to see if the calculation of the length was successful
+    if length == -1:
+        print("\nThere was an error.")
+    else:
+        print(f"The length of line {line_number} is --> {length}\n")
+
+
+#################################################################################
+# Question 2
+#################################################################################
+
+
+def checkWordCount():
+    # Asks user for file location
+    file_path = input(
+        "*  PLease enter the absolute file path to the file that you want to check the length.\n")
+    # Asks user for line number
+    line_number = input(
+        "*  Please enter the line number that you want the length for - ")
+    line_number = int(line_number)
+
+    word_count = -1  # Initialize variable to prevent errors
+
+    try:
+        file = open(file_path, "r")  # Opens the file for reading
+
+        all_lines = file.readlines()  # Gets all lines as a list of strings
+
+        num_lines = len(all_lines)  # Total of all lines in the file
+
         if num_lines < line_number:
             print(f"This file doesn't have {line_number} lines.")
         else:
             specific_line = all_lines[line_number-1]
-            length = len(specific_line)
+            all_words = specific_line.split()
+            word_count = len(all_words)
 
-    if length == 0:
+    except FileNotFoundError:
+        print("\nWrong file or file path.")
+        print("Remember to add an extension to file you entered.")
+    except IOError as e:
+        print("\nThere is an IO Error\n", e)
+    else:
+        print("No exceptions.\nUnknown Error found.")
+    finally:
+        file.close()
+
+    # Checks to see if the counting of the words were successful
+    if word_count == -1:
         print("There was an error.")
     else:
-        print(f"The length of line {line_number} is --> {length}\n")
+        print(f"The length of line {line_number} is --> {word_count}")
+
 
 #################################################################################
 # Helper Section
 #################################################################################
 
 # This function displays the menu
-
-
 def displayMenu():
     print()
     print("1. Check the length of a line from a text file.")
@@ -83,7 +140,7 @@ if __name__ == "__main__":
         elif (option == "Q" or option == "q"):
             break
         else:
-            print(f"\n \'{option}\' is not a valid option.")
+            print(f"\n\'{option}\' is not a valid option.")
             print("Please try again.")
 
         displayMenu()
